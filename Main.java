53435c9389b904d7593695f2782d9af02a17c99c
Elizabeth Izquierdo <elizabeth.izquierdo@ces.edu.uy>
2020-10-23 23:39:41 +0000

    Agregue la clase Programa

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+//package ejercicio1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Programa p=new Programa();
+		p.iniciar();
+
+	}
+
 }
